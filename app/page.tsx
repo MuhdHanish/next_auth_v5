c0d5f81c25959a800a4825d3fbfd04add040135a
MuhdHanish <muhammedhanish11@gmail.com>
2024-01-08 21:49:27 +0530

    example for server and client component using console.log

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  console.log("WHERE AM I");
   return (
     <Button size={"lg"}>
       Click me
