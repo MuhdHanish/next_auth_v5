09d394da4479cd2515ec12b26aae3ba549f84548
MuhdHanish <muhammedhanish11@gmail.com>
2024-01-09 21:19:27 +0530

    feat(prisma): initialized prisma completed postregsql integration using prisma created user model and account model checked email is using or not completed the user registrestion except email varificaiton hashed password using bcrypt

--- actions/register.ts
+++ actions/register.ts
@@ -1,5 +1,7 @@
 "use server";
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
 import { registerSchema } from "@/schemas";
 
 export interface IRegisterResponse {
@@ -13,5 +15,23 @@ export const register = async (values: z.infer<typeof registerSchema>): Promise<
     if (!validateFields.success) {
         return Promise.resolve({ status: "error", message: "Invalid credentials!" });
     }
-    return  Promise.resolve({ status: "success", message: "Email sent!" });
+    const { name, email, password } = validateFields.data;
+    const userExist = await db.user.findUnique({
+        where: {
+            email
+        }
+    });
+    if (userExist) {
+        return Promise.resolve({ status: "error", message: "Email already in use!" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    });
+    // TODO: Send verifcation token email
+    return  Promise.resolve({ status: "success", message: "Account created!" });
 };
