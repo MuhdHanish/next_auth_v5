ed23861070cae55499d478ab4510e6f1f57f9b36
MuhdHanish <muhammedhanish11@gmail.com>
2024-01-09 22:09:31 +0530

    added common user utils in one file data/user.ts

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,9 @@
 "use server";
 import * as z from "zod";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { db } from "@/lib/db";
 import { registerSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export interface IRegisterResponse {
     status: "error" | "success";
@@ -16,11 +17,7 @@ export const register = async (values: z.infer<typeof registerSchema>): Promise<
         return Promise.resolve({ status: "error", message: "Invalid credentials!" });
     }
     const { name, email, password } = validateFields.data;
-    const userExist = await db.user.findUnique({
-        where: {
-            email
-        }
-    });
+    const userExist = await getUserByEmail(email);
     if (userExist) {
         return Promise.resolve({ status: "error", message: "Email already in use!" });
     }
